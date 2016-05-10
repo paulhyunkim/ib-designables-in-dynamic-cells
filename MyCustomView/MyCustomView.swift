037ea34f2c46671c4e588965ecb37f3d6c3000ed
Paul Kim <Paul.Kim.contractor@hilton.com>
2016-05-10 14:29:10 -0700

    clean up

--- MyCustomView/MyCustomView.swift
+++ MyCustomView/MyCustomView.swift
@@ -40,17 +40,8 @@ import UIKit
     }
     
     func xibSetup() {
-//        self.alpha = 0
         view = loadViewFromNib()
-        
-        // use bounds not frame or it'll be offset
         view.frame = bounds
-        
-//        self.bounds = CGRect(x: 0, y: 0, width: 376, height: 300)
-        
-        // Make the view stretch with containing view
-        view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
-        // Adding custom subview on top of our view (over any custom drawing > see note below)
         addSubview(view)
     }
     
@@ -58,28 +49,18 @@ import UIKit
         let bundle = NSBundle(forClass: self.dynamicType)
         let nib = UINib(nibName: "MyCustomView", bundle: bundle)
         let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
-        
         return view
     }
     
     override init(frame: CGRect) {
-        // 1. setup any properties here
-        
-        // 2. call super.init(frame:)
         super.init(frame: frame)
-        
-        // 3. Setup view from .xib file
         xibSetup()
     }
     
     required public init?(coder aDecoder: NSCoder) {
-        // 1. setup any properties here
-        
-        // 2. call super.init(coder:)
         super.init(coder: aDecoder)
-        
-        // 3. Setup view from .xib file
         xibSetup()
     }
 
 }
+
--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -11,26 +11,24 @@ import MyCustomView
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    let dataSource = [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper ligula libero.",
+        "Sed euismod eleifend justo laoreet congue. Etiam ante lacus, accumsan non sodales nec, pulvinar eu dolor. Suspendisse ligula libero, congue non ante sit amet, luctus sagittis lacus. Nullam vitae dui pellentesque est faucibus scelerisque sed eget justo. Aliquam varius semper aliquet.",
+        "Aliquam dictum pretium lorem, id iaculis ex scelerisque in.",
+        "Quisque nec sem nisl. Vestibulum vitae turpis eget risus faucibus porta. Maecenas nec elit finibus, malesuada neque ac, mollis orci. Fusce eu velit cursus, suscipit libero vitae, sodales erat. Vivamus bibendum vitae justo id placerat. Integer sagittis lacinia dolor, vel lacinia arcu placerat nec. Proin leo sapien, tempus non iaculis quis, euismod rutrum ipsum. Quisque sed leo purus. Ut felis nisl, mollis sollicitudin nibh vel, iaculis blandit sem.",
+        "Nullam ultrices tellus arcu, id condimentum felis lobortis ut. Maecenas mauris metus, viverra vitae laoreet tempor, tempus sed lacus. Quisque dictum dapibus elit eu feugiat."
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
     
 extension ViewController: UITableViewDelegate {
-    
-//    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-//        return 100
-//    }
     
     func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 100
@@ -46,26 +44,17 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let array = [
-            "asdf",
-            "asdf asdf asdf asdf asdf asdf asdf",
-            "asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf",
-            "asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf",
-            "asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf"
-        ]   
+
         
         let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
-//         MyCustomView
         
         if let customView = cell.subviews[0].subviews[0] as? MyCustomView {
-//            customView.alpha = (1.0 / 100) * CGFloat(indexPath.row)
-            customView.myLabel.text = array[indexPath.row]
+            customView.myLabel.text = dataSource[indexPath.row]
         }
-        
-        
-//        customView.
     
         return cell
     }
     
 }
+    
+    
